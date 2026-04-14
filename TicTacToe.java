1328984ea4e0627d5de5501c378bd6cd47089cc4
ranjeetprasad <ranjeet.prasad@paytmpayments.com>
2026-04-14 17:11:22 +0530

    Added Main function and play

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,11 @@
-
+import java.util.*;
 
 interface Iobserver {
     void update(String msg);
 }
 
 
-class ConcreteObserver implements Iobserver {
+class ConsoleNotifier implements Iobserver {
 
     @Override
     public void update(String msg) {
@@ -109,6 +109,10 @@ class TicTacToePlayer {
         score = 0;
     }
 
+    public void incrementScore() {
+        score++;
+    }
+
     public Symbol getSymbol() {
         return symbol;
     }
@@ -133,7 +137,7 @@ interface TicTacToeRules {
 
     boolean checkWinCondition(Board board, Symbol symbol);
 
-    boolean checkDrawCondition(Board board, Symbol symbol);
+    boolean checkDrawCondition(Board board);
 }
 
 
@@ -203,7 +207,7 @@ class StandardTicTacToeRules implements TicTacToeRules {
 
     // if all cell are filled and no Winner
     @Override
-    public boolean checkDrawCondition(Board board, Symbol symbol) {
+    public boolean checkDrawCondition(Board board) {
         int n = board.getsize();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -217,7 +221,135 @@ class StandardTicTacToeRules implements TicTacToeRules {
 }
 
 
+//Game Class --> Observable
+class TicTacToeGame {
+    private Board board;
+    private Deque<TicTacToePlayer> players = new ArrayDeque<>();
+    private TicTacToeRules rules;
+    private List<Iobserver> observers;
+    private boolean gameOver;
+
+    public TicTacToeGame(int size) {
+        this.board = new Board(size);
+        observers = new ArrayList<>();
+        rules = new StandardTicTacToeRules();
+        observers = new ArrayList<>();
+        gameOver = false;
+    }
+
+    public void addPlayer(TicTacToePlayer player) {
+        players.add(player);
+    }
+
+    public void addObserver(Iobserver observer) {
+        observers.add(observer);
+    }
+
+    public void clearObservers() {
+        observers.clear();
+    }
+
+    public void notify(String msg) {
+        for (Iobserver observer : observers) {
+            observer.update(msg);
+        }
+    }
+
+    public void play() {
+        if (players.size() < 2) {
+            System.out.println("Need at least 2 players!");
+            return;
+        }
+
+        notify("Tic Tac Toe Game Started!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (!gameOver) {
+            board.display();
+
+            // Take out the current player from dequeue
+            TicTacToePlayer currentPlayer = players.peekFirst();
+            System.out.print(currentPlayer.getName() + " (" + currentPlayer.getSymbol().getmark() + ") - Enter row and column: ");
+
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            // check if move is valid
+            if (rules.isValidMove(board, row, col)) {
+                board.placeMark(row, col, currentPlayer.getSymbol());
+                notify(currentPlayer.getName() + " played (" + row + "," + col + ")");
+
+                if (rules.checkWinCondition(board, currentPlayer.getSymbol())) {
+                    board.display();
+                    System.out.println(currentPlayer.getName() + " wins!");
+                    currentPlayer.incrementScore();
+
+                    notify(currentPlayer.getName() + " wins!");
+
+                    gameOver = true;
+                } else if (rules.checkDrawCondition(board)) {
+                    board.display();
+
+                    System.out.println("It's a draw!");
+                    notify("Game is Draw!");
+
+                    gameOver = true;
+                } else {
+                    // Move player to back of queue
+                    players.removeFirst();
+                    players.addLast(currentPlayer);
+                }
+            } else {
+                System.out.println("Invalid move! Try again.");
+            }
+        }
+    }
+
+}
+
+enum GameType {
+    STANDARD
+}
+
+class TicTacToeGameFactory {
+    public static TicTacToeGame createGame(GameType gt, int boardSize) {
+        if (GameType.STANDARD == gt) {
+            return new TicTacToeGame(boardSize);
+        }
+        return null;
+    }
+}
+
+
 public class TicTacToe {
+
+    public static void main(String[] args) {
+        System.out.println("=== TIC TAC TOE GAME ===");
+
+        // Create game with custom board size
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter board size (e.g., 3 for 3x3): ");
+        int boardSize = scanner.nextInt();
+
+        TicTacToeGame game = TicTacToeGameFactory.createGame(GameType.STANDARD, boardSize);
+
+        // Add observer
+        Iobserver notifier = new ConsoleNotifier();
+        game.addObserver(notifier);
+
+        // Create players with custom symbols
+        TicTacToePlayer player1 = new TicTacToePlayer(1, "Aditya", new Symbol('X'));
+        TicTacToePlayer player2 = new TicTacToePlayer(2, "Harshita", new Symbol('O'));
+
+        game.addPlayer(player1);
+        game.addPlayer(player2);
+
+        // Play the game
+        game.play();
+
+        scanner.close();
+    }
 
 
 }
